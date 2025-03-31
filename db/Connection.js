30596751143d07dd48329512feb1b18eccea213d
NirajDX <niraj.t@designersx.com>
2025-03-31 10:39:55 +0530

    [AV] updated code here (AV1.1)

--- db/Connection.js
+++ db/Connection.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "Tasp_database",
+  database: "tasp_database",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- routes/route.js
+++ routes/route.js
@@ -2,105 +2,47 @@ const express = require("express");
 const router = express.Router();
 const { signUpApi } = require("../controllers/ChatBot/SignUp");
 const { loginApi } = require("../controllers/ChatBot/SignUp");
-const {verifyOtpApi}=require("../controllers/ChatBot/SignUp");
-const {resendOtpApi}=require("../controllers/ChatBot/SignUp");
-const {loginAdminApi}=require("../controllers/Admin/Login");
-const {chatHistory,getChatHistory,deleteChatHistory}=require("../controllers/ChatBot/chatHistory")
+const { verifyOtpApi } = require("../controllers/ChatBot/SignUp");
+const { resendOtpApi } = require("../controllers/ChatBot/SignUp");
+const { loginAdminApi } = require("../controllers/Admin/Login");
+const {
+  chatHistory,
+  getChatHistory2,
+  getChatHistory,
+  deleteChatHistory,
+} = require("../controllers/ChatBot/chatHistory");
 // const {loginAdminApi}=require("../controllers/Admin/Login")
-const { requestOtpApi, verifyadminOtpApi, resetPasswordApi } = require("../controllers/Admin/ForgotPassword");
+const {
+  requestOtpApi,
+  verifyadminOtpApi,
+  resetPasswordApi,
+} = require("../controllers/Admin/ForgotPassword");
 const { signupAdminApi } = require("../controllers/Admin/Signup");
-const { getUserInfoApi, updateUserApi } = require("../controllers/Admin/UpdateAdmin");
-const { getAllUsers, deleteUserApi } = require("../controllers/Admin/AllUsers/AllUsers"); 
+const {
+  getUserInfoApi,
+  updateUserApi,
+} = require("../controllers/Admin/UpdateAdmin");
+const {
+  getAllUsers,
+  deleteUserApi,
+} = require("../controllers/Admin/AllUsers/AllUsers");
 router.post("/sign-up", signUpApi);
 router.post("/login-api", loginApi);
-router.post("/verify-otp",verifyOtpApi);
+router.post("/verify-otp", verifyOtpApi);
 router.post("/admin-signup", signupAdminApi);
-router.post("/admin-login",loginAdminApi);
-router.post("/resend-otp",resendOtpApi)
-router.post("/admin/request-otp", requestOtpApi);  
-router.post("/admin/verify-otp", verifyadminOtpApi);    
+router.post("/admin-login", loginAdminApi);
+router.post("/resend-otp", resendOtpApi);
+router.post("/admin/request-otp", requestOtpApi);
+router.post("/admin/verify-otp", verifyadminOtpApi);
 router.post("/admin/reset-password", resetPasswordApi);
-router.get("/user/:userId", getUserInfoApi); 
-router.post("/chat_history",chatHistory);
+router.get("/user/:userId", getUserInfoApi);
+router.post("/chat_history", chatHistory);
 router.get("/get_chat_history", getChatHistory);
-router.put("/user/update/:userId", updateUserApi); 
-router.get("/users", getAllUsers); 
-router.delete("/user/delete/:userId", deleteUserApi); 
-router.delete("/delete-chat-history",deleteChatHistory)
+router.put("/user/update/:userId", updateUserApi);
+router.get("/users", getAllUsers);
+router.delete("/user/delete/:userId", deleteUserApi);
+router.delete("/delete-chat-history", deleteChatHistory);
+// router.get("/get-chat-limit-history",getLatestTwoMessages);
+// router.get("/latest-chat-history",getMessagesExcludingLatestTwo);
+router.get("/get-chat-2", getChatHistory2);
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
